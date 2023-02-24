7f83b74ddebf4340280d17c1f79e80c15d3b420a
Catherine Al Skaff <catherine.alskaf@gmail.com>
2023-02-24 10:06:12 +0100

    adding a cluster map

--- seeds/locations.js
+++ seeds/locations.js
@@ -1,49 +1,98 @@
-module.exports=[
-    {
-      title: "Summer Music Festival",
-      description: "A three-day outdoor music festival featuring a lineup of top international and local artists.",
-      location: "Amsterdam, Netherlands",
-      content: "Enjoy live performances from a variety of musical genres, including rock, pop, hip hop, and electronic dance music. Food and drink vendors will be on-site, and there will be a variety of activities for all ages. Don't miss this chance to experience the best music and fun under the summer sun!",
-      latitude: 52.3702,
-      longitude: 4.8952
-    },
-    {
-      title: "Food & Wine Celebration",
-      description: "A two-day event showcasing the finest food, wine, and spirits from around the world.",
-      location: "Paris, France",
-      content: "Sample dishes from renowned chefs, sip on exceptional wines, and discover new spirits at this exciting culinary event. Meet the winemakers and distillers, attend cooking demonstrations, and indulge in your passions for food and drink. Treat yourself to a memorable experience in the culinary capital of the world!",
-      latitude: 48.8566,
-      longitude: 2.3522
-    },
-    {
-      title: "Art & Design Exhibition",
-      description: "A five-day event featuring exhibitions and demonstrations by leading artists and designers.",
-      location: "Berlin, Germany",
-      content: "Explore a diverse range of art and design styles, from traditional paintings and sculptures to cutting-edge digital media. Watch live demonstrations from talented artists, attend insightful talks and discussions, and immerse yourself in the world of art and design. Don't miss this chance to be inspired and amazed!",
-      latitude: 52.5200,
-      longitude: 13.4050
-    },
-    {
-      title: "International Film Festival",
-      description: "An annual film festival showcasing the latest works from established and up-and-coming filmmakers.",
-      location: "Rome, Italy",
-      content: "Discover the latest in world cinema, from major Hollywood productions to independent films from around the globe. Meet filmmakers and actors, attend Q&A sessions, and enjoy a rich program of screenings, events, and parties. Immerse yourself in the world of film and experience the magic of cinema!",
-      latitude: 41.9028,
-      longitude: 12.4964
-    },
-    {
-      title: "Theatre Festival",
-      description: "A two-week festival featuring a diverse program of contemporary and classical performances.",
-      location: "London, UK",
-      content: "See a range of exciting productions, from new plays to classic works. Meet the cast and crew, attend Q&A sessions, and be inspired by the creativity and talent on display. Enjoy a dynamic program of performances, workshops, and events, and experience the excitement of the theatre!",
-      latitude: 51.5074,
-      longitude: -0.1278
-    },
-    {
-      title: "Jazz Festival",
-      description: "A three-day festival celebrating the best in jazz music.",
-      location: "New York, USA",
-      content: "Hear live performances from world-renowned jazz artists, attend workshops and masterclasses, and immerse yourself in the rich history and culture of jazz. With a lineup of talented musicians, and a range of events for all ages, this is the perfect opportunity to experience the magic of jazz!",
-      latitude: 40.7128,
-      longitude: -74.0060
-    }]
+module.exports = [
+  {
+    title: "Summer Music Festival",
+    description: "A three-day outdoor music festival featuring a lineup of top international and local artists.",
+    location: "Amsterdam, Netherlands",
+    content: "Enjoy live performances from a variety of musical genres, including rock, pop, hip hop, and electronic dance music. Food and drink vendors will be on-site, and there will be a variety of activities for all ages. Don't miss this chance to experience the best music and fun under the summer sun!",
+    latitude: 52.3702,
+    longitude: 4.8952
+  },
+  {
+    title: "Food & Wine Celebration",
+    description: "A two-day event showcasing the finest food, wine, and spirits from around the world.",
+    location: "Paris, France",
+    content: "Sample dishes from renowned chefs, sip on exceptional wines, and discover new spirits at this exciting culinary event. Meet the winemakers and distillers, attend cooking demonstrations, and indulge in your passions for food and drink. Treat yourself to a memorable experience in the culinary capital of the world!",
+    latitude: 48.8566,
+    longitude: 2.3522
+  },
+  {
+    title: "Art & Design Exhibition",
+    description: "A five-day event featuring exhibitions and demonstrations by leading artists and designers.",
+    location: "Berlin, Germany",
+    content: "Explore a diverse range of art and design styles, from traditional paintings and sculptures to cutting-edge digital media. Watch live demonstrations from talented artists, attend insightful talks and discussions, and immerse yourself in the world of art and design. Don't miss this chance to be inspired and amazed!",
+    latitude: 52.5200,
+    longitude: 13.4050
+  },
+  {
+    title: "International Film Festival",
+    description: "An annual film festival showcasing the latest works from established and up-and-coming filmmakers.",
+    location: "Rome, Italy",
+    content: "Discover the latest in world cinema, from major Hollywood productions to independent films from around the globe. Meet filmmakers and actors, attend Q&A sessions, and enjoy a rich program of screenings, events, and parties. Immerse yourself in the world of film and experience the magic of cinema!",
+    latitude: 41.9028,
+    longitude: 12.4964
+  },
+  {
+    title: "Theatre Festival",
+    description: "A two-week festival featuring a diverse program of contemporary and classical performances.",
+    location: "London, UK",
+    content: "See a range of exciting productions, from new plays to classic works. Meet the cast and crew, attend Q&A sessions, and be inspired by the creativity and talent on display. Enjoy a dynamic program of performances, workshops, and events, and experience the excitement of the theatre!",
+    latitude: 51.5074,
+    longitude: -0.1278
+  },
+  {
+    title: "Jazz Festival",
+    description: "A three-day festival celebrating the best in jazz music.",
+    location: "New York, USA",
+    content: "Hear live performances from world-renowned jazz artists, attend workshops and masterclasses, and immerse yourself in the rich history and culture of jazz. With a lineup of talented musicians, and a range of events for all ages, this is the perfect opportunity to experience the magic of jazz!",
+    latitude: 40.7128,
+    longitude: -74.0060
+  }
+  ,
+  {
+    title: "International Film Festival",
+    description: "An annual film festival showcasing the latest works from established and up-and-coming filmmakers.",
+    location: "Toronto, Canada",
+    content: "Discover the latest in world cinema, from major Hollywood productions to independent films from around the globe. Meet filmmakers and actors, attend Q&A sessions, and enjoy a rich program of screenings, events, and parties. Immerse yourself in the world of film and experience the magic of cinema!",
+    latitude: 43.6532,
+    longitude: -79.3832
+  },
+  {
+    title: "Book Fair",
+    description: "A week-long book fair featuring a variety of new and established authors and publishers.",
+    location: "Tokyo, Japan",
+    content: "Discover the latest in the world of literature and meet your favorite authors at this exciting book fair. Attend readings and panel discussions, and find new books to add to your collection. With a range of events for all ages, this is the perfect opportunity to celebrate the power of books!",
+    latitude: 35.6762,
+    longitude: 139.6503
+  },
+  {
+    title: "Fashion Week",
+    description: "A week-long celebration of fashion featuring the latest trends and designs.",
+    location: "Milan, Italy",
+    content: "See the latest collections from top designers, attend runway shows and presentations, and immerse yourself in the world of fashion. With a rich program of events, parties, and exclusive experiences, this is the perfect opportunity to discover the latest trends and meet the top players in the fashion industry!",
+    latitude: 45.4642,
+    longitude: 9.1900
+  },
+  {
+    title: "Science Fair",
+    description: "An annual science fair showcasing the latest innovations and breakthroughs in science and technology.",
+    location: "San Francisco, USA",
+    content: "Explore the latest discoveries in science and technology, from cutting-edge research to new products and technologies. Meet the scientists and innovators behind the breakthroughs, attend talks and presentations, and experience the excitement of the world of science. With a range of activities for all ages, this is the perfect opportunity to discover the wonders of science!",
+    latitude: 37.7749,
+    longitude: -122.4194
+  },
+  {
+    title: "Comedy Festival",
+    description: "A week-long festival featuring the best stand-up comedians from around the world.",
+    location: "Edinburgh, UK",
+    content: "Laugh out loud with the best stand-up comedians from around the world at this exciting comedy festival. With a diverse lineup of comedians and a range of venues, this is the perfect opportunity to experience the fun and humor of the world of comedy!",
+    latitude: 55.9533,
+    longitude: -3.1883
+  },
+  {
+    title: "Tech Conference",
+    description: "An annual tech conference featuring the latest innovations in technology and entrepreneurship.",
+    location: "Bangalore, India",
+    content: "Discover the latest in technology and entrepreneurship at this exciting tech conference. Attend talks and presentations by industry leaders, network with other entrepreneurs and investors, and explore the latest in tech products and services. With a range of activities for all levels of experience, this is the perfect opportunity to take your tech skills to the next level!",
+    latitude: 12.9716,
+    longitude: 77.5946
+  }]
